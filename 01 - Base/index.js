7fa3cd9f2bc67bbe6219bad728dc55d531ad136f
胡青杨 <i@do.codes>
2018-01-25 18:04:47 +0800

    01 - Base

---
+++ 01 - Base/index.js
@@ -0,0 +1,7 @@
+
+fetch('./target.wasm')
+    .then(response => response.arrayBuffer())
+    .then(bytes => WebAssembly.instantiate(bytes, {}))
+    .then(results => {
+        alert(results.instance.exports.add_one(41));
+    });
---
+++ 01 - Base/src/main.rs
@@ -0,0 +1,9 @@
+
+#[no_mangle]
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+fn main() {
+    println!("Hello!");
+}
