de145d4f1aa15b5b90180afeb1cf67ad9381bf44
胡青杨 <i@do.codes>
2018-01-26 21:47:45 +0800

    Using webpack

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,20 @@
 
-async function main() {
+(async () => {
     const { fib } = await import('../src/lib.rs');
-    console.log(fib(10));
-}
 
+    function fibJs(i) {
+        switch (i) {
+            case 0: return 0;
+            case 1: return 1;
+            default: return fibJs(i-1) + fibJs(i-2)
+        }
+    }
 
-main();
+    console.time('fibJs');
+    console.log(fibJs(50));
+    console.timeEnd('fibJs');
+
+    console.time('fib');
+    console.log(fib(50));
+    console.timeEnd('fib');
+})();
